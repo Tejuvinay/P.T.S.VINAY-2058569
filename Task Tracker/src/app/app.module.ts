500c0d0bd288d2f3d64eb81842a462ea68f08e04
Tejuvinay <87991633+Tejuvinay@users.noreply.github.com>
2021-08-19 16:25:28 +0000

    test

---
+++ Task Tracker/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskchComponent } from './taskch/taskch.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import{MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule, MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule, MatTableModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Task Tracker/src/app/employee.ts
@@ -0,0 +1,5 @@
+export class Employee {
+  constructor(public Id: string, public Name: string, public Task: string, public Deadline: string) {
+    
+  }
+}
