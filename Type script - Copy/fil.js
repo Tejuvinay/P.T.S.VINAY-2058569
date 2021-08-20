79d9c47768ac64b698a062da0e008aeab5eee3c7
Tejuvinay <87991633+Tejuvinay@users.noreply.github.com>
2021-08-20 10:08:08 -0400

    Delete Type script - Copy directory

--- Type script - Copy/fil.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello');
-var numbers = [1, 2, 3, 4, 5, 6, 77];
-var greaterThan = numbers.filter(function (number) { return number > 2; });
-console.log(greaterThan);
-var a = 3;
-console.log(a);
-var isAd = true;
-var str = "wegwg";
-console.log(isAd + "<br>" + str + "<br>" + a);
--- Type script - Copy/fil.ts
+++
@@ -1,9 +0,0 @@
-﻿console.log('Hello');
-let numbers = [1, 2, 3, 4, 5, 6, 77];
-let greaterThan = numbers.filter(number => number > 2);
-console.log(greaterThan);
-let a:number = 3;
-console.log(a);
-let isAd: boolean = true;
-let str: string = "wegwg";
-console.log(isAd + "<br>" + str + "<br>" + a);
